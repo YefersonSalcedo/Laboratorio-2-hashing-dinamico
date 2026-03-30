65fa6b5d610e9d033dd6aa3c0665af210efc1d37
SamuelVelasquezB <samuel.velasquezb@udea.edu.co>
2026-03-30 17:06:09 -0500

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,3 +1,28 @@
 public class Usuario {
-    
+    private long cc;
+    private String nombre;
+    private String correo;
+
+    public Usuario(long cc, String nombre, String correo) {
+        this.cc = cc;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public long getCc() {
+        return cc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CC: %-12d | Nombre: %-20s | Correo: %s", cc, nombre, correo);
+    }
 }
